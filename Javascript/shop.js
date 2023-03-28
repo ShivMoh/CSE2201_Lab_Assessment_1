0273ea23aeb3bd32c95afc0888597db5f3943b30
Shivesh Mohamed <shiveshmohamed1234@gmail.com>
2023-03-28 14:15:31 -0400

    Added some content

--- Javascript/shop.js
+++ Javascript/shop.js
@@ -2,29 +2,21 @@ var products = document.getElementsByClassName("product")
 var sortBy = document.getElementById("sort-by")
 var category =  document.getElementById("category")
 
+// there's not database to pull data to sort items
+// so this just randomly swaps the items to create some effect 
+// purely for demonstrative reasons
 sortBy.addEventListener('change', () => {
     var random = 0;
     var src
     var arr = []
     var arr2 = []
     for(var x = 0; x < products.length; x++) {
-        // while(true) {
-        //     random = Math.floor(Math.random() * products.length);
-        //     if(!arr.includes(random)) {
-        //         arr.push(random)
-        //         break;
-        //     }
-        // }
         random = Math.floor(Math.random() * products.length);
         arr.push(x)
         arr2.push(random)
         src = products[x].children[0].src
         products[x].children[0].src = products[random].children[0].src
         products[random].children[0].src = src
-        console.log(products[x].children[0].src)
-        console.log(products[random].children[0].src)
-        console.log(arr)
-        console.log(arr2)
 
     }
 })
