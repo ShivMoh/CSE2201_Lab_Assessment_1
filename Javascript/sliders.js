63b48770c5a1d49ee84c78533dfe910095f6d58a
Rey <reynardetwaroo4@gmail.com>
2023-03-29 21:31:40 -0400

    added some comments.

--- Javascript/sliders.js
+++ Javascript/sliders.js
@@ -1,3 +1,4 @@
+//Define slideshow02 object with the next button, previous button, slides, slide items, current index, and timer properties
 const slideshow02 = {
   nextButton: document.querySelector('.next02'),
   prevButton: document.querySelector('.prev02'),
@@ -16,36 +17,42 @@ const slideshow03 = {
   timer: null
 };
 
+//Define function to handle the next button click for a given slideshow
 function handleNext(slideshow) {
   slideshow.currentIndex = (slideshow.currentIndex + 1) % slideshow.slideItems.length;
   if (slideshow.currentIndex > 3) {
-    slideshow.currentIndex = 0; // use assignment operator instead of equality operator
+    slideshow.currentIndex = 0; 
   }
   slideshow.slides.style.transform = `translateX(-${slideshow.currentIndex * 600}px)`;
 }
 
+//Define function to handle the previous button click for a given slideshow
 function handlePrev(slideshow) {
   slideshow.currentIndex = (slideshow.currentIndex - 1 + slideshow.slideItems.length) % slideshow.slideItems.length;
   if (slideshow.currentIndex > 3) {
-    slideshow.currentIndex = 3; // use assignment operator instead of equality operator
+    slideshow.currentIndex = 3; 
   }
   slideshow.slides.style.transform = `translateX(-${slideshow.currentIndex * 600}px)`;
 }
 
+//Define function to start the timer for a given slidesho
 function startTimer(slideshow) {
   slideshow.timer = setInterval(() => {
     handleNext(slideshow);
   }, 7000);
 }
 
+//Define function to reset the timer for a given slideshow
 function resetTimer(slideshow) {
   clearInterval(slideshow.timer);
   startTimer(slideshow);
 }
 
+//Start the timers for both slideshows
 startTimer(slideshow02);
 startTimer(slideshow03);
 
+//Add event listener for the next button click for slideshow02
 slideshow02.nextButton.addEventListener('click', () => {
   handleNext(slideshow02);
   resetTimer(slideshow02);
